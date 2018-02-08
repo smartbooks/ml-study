72f7fe1e11fd6529be4d6bb5f065180b8e9fc5e5
wangya <wangya@zshiliu.com>
2018-02-08 19:49:02 +0800

    666

--- tensorflow-work/hello.py
+++ tensorflow-work/hello.py
@@ -26,7 +26,7 @@ sess = tf.Session()
 sess.run(init)
 
 # 拟合平面
-for step in xrange(0, 201):
+for step in range(0, 201):
     sess.run(train)
     if step % 20 == 0:
         print(step, sess.run(W), sess.run(b))
