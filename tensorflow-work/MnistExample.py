360b805e68285fd1b49f6e8c83bf7f3aaa2862b5
wangya <wangya@zshiliu.com>
2018-02-09 17:20:01 +0800

    666

---
+++ tensorflow-work/MnistExample.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow.examples.tutorials.mnist.input_data as mn
+import tensorflow as tf
+
+mnist = mn.read_data_sets("C:/test/MNIST", one_hot=True)
+
+x = tf.placeholder("float", [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+y_ = tf.placeholder("float", [None, 10])
+cross_entropy = - tf.reduce_sum(y_ * tf.log(y))
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+
+for i in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+    correct_prediction = tf.equal(tf.arg_max(y, 1), tf.arg_max(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
