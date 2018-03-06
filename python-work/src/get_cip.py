574097962fb90af048d60543f53679aff7d526df
smartbooks <smartbooks@qq.com>
2018-03-06 18:04:49 +0800

    666

--- python-work/src/get_cip.py
+++ python-work/src/get_cip.py
@@ -9,7 +9,7 @@ browser = webdriver.Chrome(chromedriver)
 url = "http://182.131.21.139/gspt/ccm-action/domesticgame/searchGame"
 browser.get(url)
 
-for x in range(1, 2216):
+for x in range(501, 2216):
     doc_list = browser.find_element_by_class_name("content-top").find_elements_by_tag_name("li")
 
     xh, bawh, yymc, pwrq = "", "", "", ""
@@ -25,14 +25,17 @@ for x in range(1, 2216):
         elif id == 'pwrq':
             pwrq = doc.text
 
-        if "0956" in bawh:
+        if "RPG 0956" in bawh:
             print(x, xh, bawh, yymc, pwrq)
             browser.quit()
+            break
 
     # 自动翻到下一页
     time.sleep(1)
-    next = browser.find_element_by_class_name("content-fy").find_element_by_link_text("下一页")
-    next.click()
+    # browser.find_element_by_class_name("content-fy").find_element_by_link_text("下一页").click()
+    page = browser.find_element_by_id("page")
+    browser.execute_script("arguments[0].value='" + str(x) + "';", page)
+    browser.find_element_by_id("queryform").submit()
 
 # 关闭浏览器
 # browser.quit()
