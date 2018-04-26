b8919610222f9a511585492d29b98434cf1247d0
smartbooks <smartbooks@qq.com>
2018-04-26 16:22:30 +0800

    666

--- python-work/src/LineRegression.py
+++ python-work/src/LineRegression.py
@@ -4,17 +4,7 @@
 import numpy as nm
 import matplotlib.pyplot as plt
 
-dataMat = nm.mat([
-    [0.478, 0.827],
-    [0.512, 0.871],
-    [0.919, 0.790],
-    [0.859, 0.059],
-    [0.675, 0.252],
-    [0.741, 0.971],
-    [0.491, 0.430],
-    [0.153, 0.624],
-    [0.560, 0.360],
-    [0.485, 0.324]])
+dataMat = nm.mat(nm.random.uniform(0, 10, size=(50, 2)))
 
 sortDataMat = dataMat.copy()
 sortDataMat.sort(0)
@@ -22,19 +12,12 @@ sortDataMat.sort(0)
 # 全1矩阵
 labelOneMat = nm.ones((sortDataMat.shape[0], 1))
 
-
-# 全0矩阵
-# labelZeroMat = nm.zeros((sortDataMat.shape[0], 1), dtype=nm.int)
-
-# 单位矩阵
-# eyeMat = nm.eye(sortDataMat.shape[1])
-
-
 # 损失函数
 def lrLoss(yMat, yHatMat, showPlot=False):
     e = yMat - yHatMat
     eSquare = nm.square(e)
     eSquareSum = nm.sum(nm.square(e))
+    print("Y:\n", yMat)
     print("预测:\n", yHatMat)
     print("残差:\n", e)
     print("残差平方:\n", eSquare)
@@ -66,8 +49,8 @@ def standRegres(xMat, yMat):
     xTx = xMat.T * xMat
     if nm.linalg.det(xTx) == 0.0:
         return
-    # return xTx.I * (xMat.T * yMat)
-    return nm.linalg.solve(xTx, xMat.T * yMat)
+    return xTx.I * (xMat.T * yMat)
+    # return nm.linalg.solve(xTx, xMat.T * yMat)
 
 
 # 局部加权线性回归
@@ -91,9 +74,9 @@ def lwlr(testPoint, xMat, yMat, k=1.0):
 
 def lwlrTest(xMat, yMat, k=1.0):
     m = nm.shape(xMat)[0]
-    yHat = nm.zeros(m)
+    yHat = nm.mat(nm.zeros((m, 1)))
     for i in range(m):
-        yHat[i] = lwlr(xMat[i, :], xMat, yMat, k)
+        yHat[i, 0] = lwlr(xMat[i, :], xMat, yMat, k)
     return yHat
 
 
@@ -108,11 +91,11 @@ def test1():
 def test2():
     print("**********局部加权线性回归**********")
     y = lwlrTest(sortDataMat, labelOneMat, 0.33)
-    lrLoss(labelOneMat.T, y)
+    lrLoss(labelOneMat, y, True)
 
 
 print("数据:\n", sortDataMat)
 print("标签:\n", labelOneMat)
 
 test1()
-# test2()
+test2()
