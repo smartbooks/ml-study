a3b955d4d27ef13795c510959095b5727e7a1831
smartbooks <smartbooks@qq.com>
2018-04-26 15:40:31 +0800

    666

--- python-work/src/LineRegression.py
+++ python-work/src/LineRegression.py
@@ -2,6 +2,7 @@
 # 线性回归:标准线性回归&局部加权线性回归
 
 import numpy as nm
+import matplotlib.pyplot as plt
 
 dataMat = nm.mat([
     [0.478, 0.827],
@@ -15,25 +16,49 @@ dataMat = nm.mat([
     [0.560, 0.360],
     [0.485, 0.324]])
 
+sortDataMat = dataMat.copy()
+sortDataMat.sort(0)
+
 # 全1矩阵
-labelOneMat = nm.ones((dataMat.shape[0], 1))
+labelOneMat = nm.ones((sortDataMat.shape[0], 1))
+
 
 # 全0矩阵
-labelZeroMat = nm.zeros((dataMat.shape[0], 1), dtype=nm.int)
+# labelZeroMat = nm.zeros((sortDataMat.shape[0], 1), dtype=nm.int)
 
 # 单位矩阵
-eyeMat = nm.eye(dataMat.shape[1])
+# eyeMat = nm.eye(sortDataMat.shape[1])
 
 
 # 损失函数
-def lrLoss(yMat, yHatMat):
+def lrLoss(yMat, yHatMat, showPlot=False):
     e = yMat - yHatMat
     eSquare = nm.square(e)
     eSquareSum = nm.sum(nm.square(e))
     print("预测:\n", yHatMat)
     print("残差:\n", e)
     print("残差平方:\n", eSquare)
-    print("残差平方和:\n", eSquareSum)
+    print("残差平方和:", eSquareSum)
+
+    if showPlot == True:
+        x = sortDataMat[:, 0].flatten().A[0]
+        y = sortDataMat[:, 1].flatten().A[0]
+
+        print("x =", x)
+        print("y =", y)
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.scatter(x, y)
+
+        yLab = sortDataMat[:, 0].flatten().A[0]
+        yHat = yHatMat[:, 0].flatten().A[0]
+
+        print("yLab = ", yLab)
+        print("yHat = ", yHat)
+
+        ax.plot(yHat, yLab)
+        plt.show()
 
 
 # 标准线性回归
@@ -74,19 +99,20 @@ def lwlrTest(xMat, yMat, k=1.0):
 
 def test1():
     print("**********标准线性回归**********")
-    ws = standRegres(dataMat, labelOneMat)
+    ws = standRegres(sortDataMat, labelOneMat)
+    y = sortDataMat * ws
     print("权重:\n", ws)
-    lrLoss(labelOneMat, dataMat * ws)
+    lrLoss(labelOneMat, y, True)
 
 
 def test2():
     print("**********局部加权线性回归**********")
-    y = lwlrTest(dataMat, labelOneMat, 0.33)
+    y = lwlrTest(sortDataMat, labelOneMat, 0.33)
     lrLoss(labelOneMat.T, y)
 
 
-print("数据:\n", dataMat)
+print("数据:\n", sortDataMat)
 print("标签:\n", labelOneMat)
 
 test1()
-test2()
+# test2()
