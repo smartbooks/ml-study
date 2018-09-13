4c9f248b17fe108637f40118049c4a76a858daef
smartbooks <smartbooks@qq.com>
2018-09-13 18:08:55 +0800

    666

---
+++ python-work/src/wave_parse.py
@@ -0,0 +1,42 @@
+# encoding:utf-8
+
+#https://www.cnblogs.com/lzxwalex/p/6922099.html
+#https://blog.csdn.net/qq_34971175/article/details/79485665
+
+import wave as we
+import numpy as np
+import matplotlib.pyplot as plt
+
+audio_path = '/Users/wangya/work/github.com/ml-study/python-work/src/data/test_wave.wav'
+
+def test_read():
+    print open(audio_path, 'rb').read(4)
+
+def wave_read():
+    wavfile =  we.open(audio_path,'rb')
+    params = wavfile.getparams()
+    framesra,frameswav= params[2],params[3]
+    datawav = wavfile.readframes(frameswav)
+    wavfile.close()
+    datause = np.fromstring(datawav,dtype = np.short)
+    datause.shape = -1,2
+    datause = datause.T
+    time = np.arange(0, frameswav) * (1.0/framesra)
+    return datause,time
+
+def main():
+    wavdata, wavtime = wave_read()
+    plt.title("test_wave.wav's Frames")
+    plt.subplot(211)
+    plt.plot(wavtime, wavdata[0],color = 'green')
+    plt.subplot(212)
+    plt.plot(wavtime, wavdata[1])
+    plt.show()
+
+test_read()
+
+wave_read()
+
+main()
+
+
